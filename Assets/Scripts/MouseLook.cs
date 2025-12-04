beb0459dc62efd769535abf6a9c397e1850baa63
Deynuh <60407390+Deynuh@users.noreply.github.com>
2025-12-04 00:32:54 -0800

    implemented mouse look

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    private float mouseSensitivity = 800f;
+    private float currentVerticalRotation;
+    private float currentHorizontalRotation;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // locks cursor to center of screen
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // calculate new rotations
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // clamp vertical rotation
+        currentVerticalRotation -= mouseY;
+        currentVerticalRotation = Mathf.Clamp(currentVerticalRotation, -45f, 20f);
+
+        // clamp horizontal rotation
+        currentHorizontalRotation += mouseX;
+        currentHorizontalRotation = Mathf.Clamp(currentHorizontalRotation, -180f, 0f);
+
+        // apply rotations to transform
+        transform.rotation = Quaternion.Euler(currentVerticalRotation, currentHorizontalRotation, 0f);
+    }
+}
